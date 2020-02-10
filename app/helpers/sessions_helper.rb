21e75383fa601c6616ac39f7a5b0673ce5a72c9d
Tomo OSHIMA <tomo.oshima0828@outlook.com>
2020-02-10 05:18:21 +0000

    8.3 done

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -45,4 +45,12 @@ module SessionsHelper
     !current_user.nil?
   end  
 
+  def redirect_back_or (default_url)
+    redirect_to (session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
