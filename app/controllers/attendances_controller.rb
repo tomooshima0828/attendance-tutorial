f3033ebc9b854c98082dfe01aaa1d926368c9993
Tomo OSHIMA <tomo.oshima0828@outlook.com>
2020-03-10 23:21:28 +0000

    11.1.3 done

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,9 @@
 class AttendancesController < ApplicationController
-  
+    
+    before_action :set_user, only: :edit_one_month
+    before_action :logged_in_user, only: [:update, :edit_one_month]
+    before_action :set_one_month, only: :edit_one_month
+    
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
   
   def update
@@ -22,4 +26,8 @@ class AttendancesController < ApplicationController
       end
       redirect_to @user
   end
+  
+  def edit_one_month
+  end
+  
 end
